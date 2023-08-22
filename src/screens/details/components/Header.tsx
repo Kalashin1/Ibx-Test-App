5164f540dd8b5d81bbbf2add6eef7db7bf953f56
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2023-08-22 09:59:29 +0100

    added api for details

--- src/screens/details/components/Header.tsx
+++ src/screens/details/components/Header.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import {View, Image, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/EvilIcons';
+import {DetailsProps} from '..';
 import styles from '../styles';
 
 type HeaderProps = {
   coverImage: any;
+  navigation: DetailsProps['navigation'];
 };
 
-const Header: React.FC<HeaderProps> = ({ coverImage }) => {
+const Header: React.FC<HeaderProps> = ({coverImage, navigation}) => {
   return (
     <View style={styles.headerContainer}>
       <View>
+        <TouchableOpacity onPress={navigation.goBack}>
           <View style={styles.backButton}>
-            <TouchableOpacity>
-              <Icon name="chevron-left" color="#000" size={30} />
-            </TouchableOpacity>
+            <Icon name="chevron-left" color="#000" size={30} />
           </View>
+        </TouchableOpacity>
         <View style={styles.imageContainer}>
           <Image style={styles.image} source={coverImage} />
         </View>
--- src/screens/details/index.tsx
+++ src/screens/details/index.tsx
@@ -2,31 +2,24 @@ import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 import Header from './components/Header';
 import NewsText from './components/News-Text';
+import {RootStackParamList, SCREEN_NAMES} from '../../navigation/constants';
+import type {NativeStackScreenProps} from '@react-navigation/native-stack';
 
-const data = {
-  content: `LONDON — Cryptocurrencies “have no intrinsic value” and people who invest in them should be prepared to lose all their money, Bank of England Governor Andrew Bailey said.
+export type DetailsProps = NativeStackScreenProps<
+  RootStackParamList,
+  SCREEN_NAMES.DETAIL
+>;
 
-  Digital currencies like bitcoin, ether and even dogecoin have been on a tear this year, reminding some investors of the 2017 crypto bubble in which bitcoin blasted toward $20,000, only to sink as low as $3,122 a year later.
-
-  Asked at a press conference Thursday about the rising value of cryptocurrencies, Bailey said: “They have no intrinsic value. That doesn’t mean to say people don’t put value on them, because they can have extrinsic value. But they have no intrinsic value.”
-
-  “I’m going to say this very bluntly again,” he added. “Buy them only if you’re prepared to lose all your money.”`,
-  coverImage: require('../../../assets/images/christian-buehner-Irh2teUibVE-unsplash.jpg'),
-  date: 'Sunday, 9 May 2021',
-  title:
-    'Crypto investors should be prepared to lose all their money, BOE governor says',
-  author: 'Ryan Browne',
-};
-
-const Details: React.FC = () => {
+const Details = ({navigation, route}: DetailsProps) => {
+  const data = route.params;
   return (
     <SafeAreaView>
-      <Header coverImage={data.coverImage} />
+      <Header navigation={navigation} coverImage={data.urlToImage} />
       <View>
         <NewsText
           title={data.title}
           content={data.content}
-          date={data.date}
+          date={data.publishedAt}
           author={data.author}
         />
       </View>
